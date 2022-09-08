68b1cd3b4503f84a3c521df9914f120a76ecb422
j37hr0 <jethro.cotton3@gmail.com>
2022-09-08 11:32:32 +0200

    finished all calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(i, j) {
+	return i+j;
 };
 
-const subtract = function() {
-	
+const subtract = function(i, j) {
+	return i-j;
 };
 
-const sum = function() {
-	
+const sum = function(r) {
+  return r.reduce((accumulator, curr) => accumulator + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  return ar.length
+      ? ar.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
+  
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = n;
+  if (n === 0 || n === 1){
+    return 1;
+  }
+  else if (n > 1) {
+    while (n > 1) {
+    n--;
+    result *= n;
+  }
+return result;}
 };
 
 // Do not edit below this line
