448d93c59e0b6d4ffb3b0f8d715acd59cd715a57
j37hr0 <jethro.cotton3@gmail.com>
2022-09-08 13:51:43 +0200

    finished exercise 12 findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(r) {
+const oldest = r.sort(function(a, b){
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    if (lastGuy > nextGuy) {
+        return -1;
+    }
+    else if (nextGuy > lastGuy) {
+        return 1;
+    }
+})
+return oldest[0]
 };
 
 // Do not edit below this line
