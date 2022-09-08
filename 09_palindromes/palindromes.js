63efea9afaac86cb762d4a9f6b628b86790ce0a4
j37hr0 <jethro.cotton3@gmail.com>
2022-09-08 12:00:04 +0200

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let badChars = /[^A-Za-z0-9]/g;
+    var r = string.toLowerCase().replace(badChars,"");
+    var reversedString = r.split("").reverse().join("");
+    if (reversedString === r){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
